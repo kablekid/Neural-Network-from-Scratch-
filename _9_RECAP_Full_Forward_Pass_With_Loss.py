919d2b3117e0c3cf08463634eaca802cb6fec116
Kidus Berihun <67029622+kablekid@users.noreply.github.com>
2024-10-05 23:50:12 +0300

    fixed minor errors

--- _9_RECAP_Full_Forward_Pass_With_Loss.py
+++ _9_RECAP_Full_Forward_Pass_With_Loss.py
@@ -1,16 +1,22 @@
-import nnfs
+import os, psutil
+
+import nnfs 
 import nnfs.datasets
 import numpy as np
 
 
 
+
 class DenseLayer:
-    def __init__(self,number_of_features,number_of_neurons) -> None:
+    def __init__(self,number_of_features,number_of_neurons) -> None:  # inititation of weights and bias
         self.weights =  0.01 * np.random.randn(number_of_features,number_of_neurons)
         self.bias = np.zeros((1,number_of_neurons))
 
-    def forward(self,inputs):
+    def forward(self,inputs):  # sets self.output to dot product
         self.output = np.dot(inputs,self.weights) + self.bias
+
+    def samples(self,inputs): # sets self.output to the input data
+        self.output = inputs
 
 
 
@@ -33,13 +39,16 @@ class Loss:
         # If Expected_TrueValue is a 1D array 
         if len(Expected_TrueValue.shape) == 1:
             # Select the predicted confidence values for the correct class
-            correct_confidences = pred_clipped[range(len(pred_clipped)), Expected_TrueValue]
+            # It selects from row Number (range(len(pre_clipped)))  to column (expected True Values) Eg row 0 column 1  then row 0 column 2
+            correct_confidences = pred_clipped[range(len(pred_clipped)), Expected_TrueValue]   
+            
         
 
         # If Expected_TrueValue is a 2D array (one-hot encoded labels)
         elif len(Expected_TrueValue.shape) == 2:
             # Calculate the correct confidences for one-hot encoded labels
             correct_confidences = np.sum(pred_clipped * Expected_TrueValue, axis=1)
+            
         
 
         # Calculate the loss
@@ -60,15 +69,11 @@ class Loss:
 nnfs.init()
 X,Y = nnfs.datasets.spiral_data(samples=100,classes=3) # Create spiral data set for training
 
-
-
-
-
 # we are going to have 1 input layer(two neurons), 2 hidden layer(4 neruons each) and 1 output layer(with 2 output neuron)
 loss = Loss() 
 
 InputLayer = DenseLayer(2,2)
-InputDenseLayerActivaiton = Activation()
+
 
 
 HiddenLayer1 = DenseLayer(2,4)
@@ -82,10 +87,9 @@ OutputDenseLayerActivaiton = Activation()
 
 # starting forward pass
 
-InputLayer.forward(X)
-InputDenseLayerActivaiton.Relu_forward(InputLayer.output)
+InputLayer.samples(X)
 
-HiddenLayer1.forward(InputDenseLayerActivaiton.output)
+HiddenLayer1.forward(InputLayer.output)
 HiddenDenseLayer1Activation.Relu_forward(HiddenLayer1.output)
 
 
@@ -93,10 +97,18 @@ HiddenLayer2.forward(HiddenDenseLayer1Activation.output)
 HiddenDenseLayer2Activation.Relu_forward(HiddenLayer2.output)
 
 OutputLayer.forward(HiddenDenseLayer2Activation.output)
+
+OutputLayer.output = np.array([[0.5586645,0.4660373,0.4161023]])
+
 OutputDenseLayerActivaiton.SoftMax_forward(OutputLayer.output)
 
-loss_Value = (loss.Loss_CategoricalCrossentropy(OutputDenseLayerActivaiton.output,Y))
+
+
+
+
+loss_Value = loss.Loss_CategoricalCrossentropy(OutputDenseLayerActivaiton.output,Y)
 AverageLoss = loss.Mean_Loss()
+
 
 #argmax return 
 accuracy = np.mean( np.argmax(OutputDenseLayerActivaiton.output, axis=1) == Y)
@@ -106,3 +118,8 @@ accuracy = np.mean( np.argmax(OutputDenseLayerActivaiton.output, axis=1) == Y)
 print("Loss : {} ".format(AverageLoss))
 print("Accuracy : {}% ".format((accuracy*100).round(3)))
 
+
+
+
+
+print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2, "MB")
